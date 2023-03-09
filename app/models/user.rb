d47110d5ab4d193cfebb3f9573f0ce0760ba2409
kinjoyoshiki557188 <yk.w_w.mth070188@icloud.com>
2023-03-09 14:38:40 +0900

    　パスワードが半角英数字混合での入力が必須の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers'
 validates :nickname, presence: true
 validates :last_name, presence: true
 validates :first_name, presence: true
