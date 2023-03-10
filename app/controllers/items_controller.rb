5ab29f81192cdbbed74f5443a4fa04b4253435b9
kinjoyoshiki557188 <yk.w_w.mth070188@icloud.com>
2023-03-10 17:54:56 +0900

    itemモデル作成、itemモデルのデータベース作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
 
+  def create
+    @item = create.new(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
 end
