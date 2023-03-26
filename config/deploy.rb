d5d49844e1939ccb49913e6380ac6f470caef6e0
kinjoyoshiki557188 <yk.w_w.mth070188@icloud.com>
2023-03-26 16:33:14 +0900

    自動デプロイの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'furima-38984'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:kinjoyoshiki557188/furima-38984.git'
-
+set :branch, "main"
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
